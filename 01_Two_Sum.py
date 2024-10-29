4793cebcac7cb0bb7f8a7d2626640090b2ecf331
Ansa Baby <81709896+AnsaBaby@users.noreply.github.com>
2024-10-29 13:32:52 +0000

    Update 01_Two_Sum.py

--- 01_Two_Sum.py
+++ 01_Two_Sum.py
@@ -1,13 +1,21 @@
-class Solution:
-    def twoSum(self, nums, target):
-        output = [0, 1]
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    output[0] = i
-                    output[1] = j
-                    break
-        return output
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] output= new int[2];
+        for( int i=0; i<nums.Length; i++)
+        {
+             for( int j=i+1; j<nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        output[0]= i;
+                        output[1]=j;
+                        break;
+                    }
+                }
+         }
+         return output;
+    } 
+}
     
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
